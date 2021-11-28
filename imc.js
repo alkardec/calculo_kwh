ad2180c0b409bc61ce4e75a3b53174cb549ca305
alkardec <95109746+alkardec@users.noreply.github.com>
2021-11-28 19:33:20 -0300

    Adicionar arquivos Projeto

---
+++ imc.js
@@ -0,0 +1,14 @@
+function calcular(anterior, atual){
+    var anterior = document.getElementById('txtAnterior').value;
+    var atual = document.getElementById('txtAtual').value;
+    var kwhconsumido = atual - anterior;
+    document.getElementById('resultado').innerHTML = kwhconsumido.toFixed(2);
+
+    var uso = kwhconsumido * 0.34895;
+    var energia = kwhconsumido * 0.32662;
+    var soma01 = (uso + energia) + 21.47;
+    var pis = (soma01 * 0.72) / 100;
+    var cofins = (soma01 * 3.3) / 100;
+    var valorpagar = uso + energia + 21.47 + pis + cofins + 9.66;
+    document.getElementById('resultado2').innerHTML = valorpagar.toFixed(2);
+}
